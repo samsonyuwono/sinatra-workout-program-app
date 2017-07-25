9b16307d854924c7462532c8517c55af15eb0319
Samson Yuwono <samson.yuwono@gmail.com>
2017-07-25 01:17:24 +0000

    removed unused method from exercise model

--- app/models/exercises.rb
+++ app/models/exercises.rb
@@ -4,9 +4,4 @@ class Exercise < ActiveRecord::Base
   validates :repetition, presence: true
   validates :sets, presence: true
 
-  # def self.valid_params?(params)
-  #   !params[:name].empty? && !params[:repetition].empty? &&
-  #   !params[:sets].empty?
-  #   binding.pry
-  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,7 @@
 bob = User.create(username: 'Bob', password: 'password')
 
-Exercise.create(name: 'squats', repetition: 6, sets: 4)
-Exercise.create(name: 'bench press', repetition: 6, sets: 4)
-Exercise.create(name: 'deadlift', repetition: 5, sets: 5)
 
 samson= User.create(username: 'Samson', password: 'password')
 
-Exercise.create(name: 'burpees', repetition: 10, sets: 5)
-Exercise.create(name: 'DB fly', repetition: 10, sets: 3)
 
 dummy= User.create(username: 'Dummy', password: '1234')
-
-Exercise.create(name: 'pull ups', repetition: 6, sets: 3)
-Exercise.create(name: 'push ups', repetition: 20, sets: 6)
