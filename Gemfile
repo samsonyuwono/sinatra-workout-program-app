4c97490c2016699716ff72c2adc2a59bbcf9f3dc
github username <github email address>
2018-03-14 13:41:38 -0400

    for heroku

--- Gemfile
+++ Gemfile
@@ -5,13 +5,18 @@ gem 'activerecord', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'rake'
 gem 'require_all'
-# gem 'sqlite3'
+gem 'sqlite3'
 gem 'thin'
 gem 'shotgun'
 gem 'pry'
 gem 'bcrypt'
 gem 'tux'
 gem 'pg', '~> 0.21'
+gem 'dm-core'
+gem 'haml'
+gem 'dm-postgres-adapter'
+gem 'dm-migrations'
+
 
 group :test do
   gem 'rspec'
@@ -21,6 +26,6 @@ group :test do
   gem 'rack-flash3'
 end
 
-group :development do
-  gem 'sqlite3'
-end
+# group :development do
+#   gem 'sqlite3'
+# end
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,17 @@
+require 'sinatra'
+require 'dm-core'
+require 'dm-migrations'
+
+configure do
+  # Heroku has some valuable information in the environment variables.
+  # DATABASE_URL is a complete URL for the Postgres database that Heroku
+  # provides for you, something like: postgres://user:password@host/db, which
+  # is what DM wants. This is also a convenient check whether we're in production
+  # / not.
+  DataMapper.setup(:default, (ENV["HEROKU_POSTGRESQL_ORANGE_URL"] || "sqlite3:///#{Dir.pwd}/development.sqlite3"))
+  DataMapper.auto_upgrade!
+end
+
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
